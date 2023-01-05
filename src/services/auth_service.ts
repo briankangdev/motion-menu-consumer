f700e372baf66265e269552d6da2e14d4bc3088a
IanMuler <yoelmuler@gmail.com>
2023-01-05 16:33:28 -0300

    Change user store pattern

--- src/services/auth_service.ts
+++ src/services/auth_service.ts
@@ -3,7 +3,16 @@ import {
   PUBLIC_AUTH0_DOMAIN,
   PUBLIC_AUTH0_APP_CLIENT_ID,
 } from "$env/static/public";
-import userStore from "../stores/user_store";
+import {
+  email,
+  exp,
+  iss,
+  is_authenticated,
+  jwt_token,
+  popup_opened,
+  sub,
+  name,
+} from "../stores/user_store";
 
 async function createClient() {
   let auth0Client = await createAuth0Client({
@@ -19,24 +28,24 @@ async function createClient() {
 }
 
 async function loginWithPopup(client: Auth0Client, options?: any) {
-  userStore.popup_opened.set(true);
+  popup_opened.set(true);
 
   try {
     await client.loginWithPopup(options);
     const user = await client.getUser();
     const token = await client.getTokenSilently();
 
-    userStore.is_authenticated.set(true);
-    userStore.jwt_token.set(token);
-    userStore.sub.set(user?.sub);
-    userStore.iss.set(user?.iss);
-    userStore.exp.set(user?.exp);
-    userStore.email.set(user?.email);
-    userStore.name.set(user?.name);
+    is_authenticated.set(true);
+    jwt_token.set(token);
+    sub.set(user?.sub);
+    iss.set(user?.iss);
+    exp.set(user?.exp);
+    email.set(user?.email);
+    name.set(user?.name);
   } catch (e) {
     console.error(e);
   } finally {
-    userStore.popup_opened.set(false);
+    popup_opened.set(false);
   }
 }
 
--- src/stores/user_store.ts
+++ src/stores/user_store.ts
@@ -2,24 +2,13 @@ import { derived, writable, type Writable } from "svelte/store";
 
 type Nullable<T> = T | undefined | null;
 
-class UserStore {
-  public is_authenticated: Writable<boolean> = writable(false);
-  public jwt_token: Writable<Nullable<string>> = writable();
-  public sub: Writable<Nullable<string>> = writable();
-  public exp: Writable<Nullable<string>> = writable();
-  public iss: Writable<Nullable<string>> = writable();
+export const is_authenticated: Writable<boolean> = writable(false);
+export const jwt_token: Writable<Nullable<string>> = writable();
+export const sub: Writable<Nullable<string>> = writable();
+export const exp: Writable<Nullable<string>> = writable();
+export const iss: Writable<Nullable<string>> = writable();
+export const email: Writable<Nullable<string>> = writable();
+export const name: Writable<Nullable<string>> = writable();
+export const popup_opened: Writable<boolean> = writable(false);
 
-  public email: Writable<Nullable<string>> = writable();
-  public name: Writable<Nullable<string>> = writable();
-
-  public popup_opened: Writable<boolean> = writable(false);
-
-  constructor() {}
-
-  get user_id() {
-    // sub is identical to the user id
-    return derived(this.sub, ($sub) => $sub);
-  }
-}
-
-export default new UserStore();
+export const user_id = derived(sub, ($sub) => $sub);
