0de143b4e1b1d834493bd982c5be6e6d595283a0
IanMuler <yoelmuler@gmail.com>
2023-03-08 15:54:47 -0300

    Auth services

--- src/services/auth_service.ts
+++ src/services/auth_service.ts
@@ -15,7 +15,9 @@ import {
 } from "../stores/user_store";
 
 async function createClient() {
-  let auth0Client = await createAuth0Client({
+  let auth0Client: Auth0Client;
+
+  auth0Client = await createAuth0Client({
     domain: PUBLIC_AUTH0_DOMAIN,
     clientId: PUBLIC_AUTH0_APP_CLIENT_ID,
     authorizationParams: {
@@ -32,16 +34,7 @@ async function loginWithPopup(client: Auth0Client, options?: any) {
 
   try {
     await client.loginWithPopup(options);
-    const user = await client.getUser();
-    const token = await client.getTokenSilently();
-
-    is_authenticated.set(true);
-    jwt_token.set(token);
-    sub.set(user?.sub);
-    iss.set(user?.iss);
-    exp.set(user?.exp);
-    email.set(user?.email);
-    name.set(user?.name);
+    await updateStore(client);
   } catch (e) {
     console.error(e);
   } finally {
@@ -53,10 +46,30 @@ function logout(client: Auth0Client) {
   return client.logout();
 }
 
+async function getSession(client: Auth0Client) {
+  if (await client.isAuthenticated()) {
+    await updateStore(client);
+  }
+}
+
+async function updateStore(client: Auth0Client) {
+  const user = await client.getUser();
+  const token = await client.getTokenSilently();
+
+  is_authenticated.set(true);
+  jwt_token.set(token);
+  sub.set(user?.sub);
+  iss.set(user?.iss);
+  exp.set(user?.exp);
+  email.set(user?.email);
+  name.set(user?.name);
+}
+
 const auth = {
   createClient,
   loginWithPopup,
   logout,
+  getSession,
 };
 
 export default auth;
