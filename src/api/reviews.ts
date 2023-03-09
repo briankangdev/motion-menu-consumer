e1f4cd4593d9beca4bb1b15321565ad7d02be42b
IanMuler <yoelmuler@gmail.com>
2023-03-09 17:56:44 -0300

    Reviews api update

--- src/api/reviews.ts
+++ src/api/reviews.ts
@@ -1,25 +1,25 @@
 import client from "./client";
-import type { CompanySlug } from "src/stores/company";
+import type { ICompany } from "src/stores/company";
 
 export const createReview = async (
-  company_id: CompanySlug,
-  token: string,
-  comment: string
+  comment: string,
+  company_id: ICompany["id"],
+  token: string
 ) => {
   const response = await client.post(
     `/api/v1/users/reviews`,
     {
       review: {
-        comment: comment,
+        body: comment,
         company_id: company_id,
       },
     },
     {
       headers: {
-        Authorization: token,
+        Authorization: `Bearer ${token}`,
       },
     }
   );
 
-  return response.data;
+  return response;
 };
