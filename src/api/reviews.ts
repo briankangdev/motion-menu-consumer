fef6b05f6294745788e72ebeb62c430a494b5acb
IanMuler <yoelmuler@gmail.com>
2023-03-29 16:14:40 -0300

    Add getReviews to API

--- src/api/reviews.ts
+++ src/api/reviews.ts
@@ -1,19 +1,54 @@
 import client from "./client";
+import type { AxiosResponse } from "axios";
 import type { ICompany } from "src/stores/company";
+import type { IReview } from "src/stores/reviews";
+
+export interface IResponseReviews {
+  data: { [key: string]: IReview };
+  ids: string[];
+  meta: { [key: string]: any };
+}
+
+export const getReviews = async (
+  company_id: ICompany["id"],
+  per_page: number = 10,
+  page: number = 1,
+  featured: boolean = false
+): Promise<IResponseReviews | undefined> => {
+  try {
+    const response: AxiosResponse<IResponseReviews> = await client.get(
+      `/api/v1/${company_id}/reviews`,
+      {
+        params: {
+          per_page,
+          page,
+          featured,
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const createReview = async (
   comment: string,
   company_id: ICompany["id"]
-) => {
+): Promise<IReview | undefined> => {
   try {
-    const response = await client.post(`/api/v1/users/reviews`, {
-      review: {
-        body: comment,
-        company_id: company_id,
-      },
-    });
+    const response: AxiosResponse<IReview> = await client.post(
+      `/api/v1/users/reviews`,
+      {
+        review: {
+          body: comment,
+          company_id: company_id,
+        },
+      }
+    );
 
-    return response;
+    return response.data;
   } catch (error) {
     console.error(error);
   }
