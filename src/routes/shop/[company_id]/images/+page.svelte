f63efe0e3b83e26d229b06bc936e655800daf1f6
IanMuler <yoelmuler@gmail.com>
2023-05-16 19:37:13 -0300

    Images page

--- src/routes/shop/[company_id]/images/+page.svelte
+++ src/routes/shop/[company_id]/images/+page.svelte
@@ -1,19 +1,66 @@
 <script lang="ts">
   import { _ } from "svelte-i18n";
-  import Logo from "../../../../components/Logo.svelte";
+  import { goto } from "$app/navigation";
+  import { onMount } from "svelte";
+  import { company } from "../../../../stores/company.js";
   import {
     dic as products_dic,
-    ids_with_images,
-    ids_with_videos,
-  } from "../../../../stores/products.js";
-  import { company } from "../../../../stores/company.js";
+    query,
+    grouped_by_tags,
+    filtered_ids,
+  } from "../../../../stores/products";
+  import { reviews } from "../../../../stores/reviews";
+  import Logo from "../../../../components/Logo.svelte";
+  import Button from "../../../../components/button/Button.svelte";
+  import Review from "../../../../components/review/Review.svelte";
   import Masonry from "../../../../components/Masonry.svelte";
-  import Card from "../../../../components/Card.svelte";
-  import Video from "../../../../components/Video.svelte";
+  import ProductCard from "../../../../components/cards/product-card/ProductCard.svelte";
+  import Products from "./Products.svelte";
 
-  // Fetch products data given shop company_id
   export let data;
   let company_id = data.company_id;
+
+  //reviews section
+  const REVIEWS_DISPLAYED_COUNT = 5;
+
+  let reviews_displayed = $reviews.slice(0, REVIEWS_DISPLAYED_COUNT);
+
+  //menu section
+  $: ordered_tags = $company.tag_priority
+    ? $company.tag_priority
+        .split(",")
+        .map((tag) => tag.toLowerCase().trim())
+        .filter((v, i, a) => a.indexOf(v) === i)
+    : [];
+
+  $: unordered_tags = Object.keys($grouped_by_tags).filter(
+    (key) => !ordered_tags.includes(key)
+  );
+
+  $: all_tags = ordered_tags.concat(unordered_tags);
+
+  //styles
+  let review_buttons_width =
+    $reviews.length > 0 ? "160px" : "calc(100% - 30px)";
+
+  //refs to adjust the height of the masonry
+  let header_ref = null;
+  let products_ref = null;
+  let reviews_ref = null;
+  let menu_ref = null;
+
+  //when the page is mounted, adjust the height of the menu
+  onMount(() => {
+    if (header_ref && products_ref && reviews_ref) {
+      let header_height = header_ref.getBoundingClientRect().height;
+      let products_height = products_ref.getBoundingClientRect().height;
+      let reviews_height = reviews_ref.getBoundingClientRect().height;
+
+      let max_height = header_height + reviews_height;
+
+      menu_ref.style.height = `${max_height - products_height}px`;
+    }
+  });
 </script>
 
 <svelte:head>
@@ -28,72 +75,150 @@
   />
 </svelte:head>
 
-<main>
-  <div class="header">
+<div class="container">
+  <div class="logo">
     <Logo />
+  </div>
+  <main>
+    <div class="left-desktop-section">
+      <div class="header" bind:this={header_ref}>
+        {#if $company.name}
+          <div class="row">
+            <h1>{$company.name}</h1>
+          </div>
 
-    {#if $company.name}
-      <div class="row">
-        <h1>{$company.name}</h1>
+          <p>
+            {#if $company.description}
+              {$company.description}
+            {/if}
+          </p>
+        {/if}
       </div>
 
-      <p>
-        {#if $company.description}
-          {$company.description}
+      <div class="mobile-products">
+        <Products {company_id} />
+      </div>
+
+      <section class="reviews" bind:this={reviews_ref}>
+        {#if $reviews.length > 0}
+          {#each reviews_displayed as review}
+            <div class="review">
+              <Review
+                name={review.user.name}
+                body={review.body}
+                created_at={review.created_at}
+              />
+            </div>
+          {/each}
         {/if}
-      </p>
-    {/if}
-
-    <div class="row">
-      <h5>{$_("featured_products")}</h5>
-      <a href={`/shop/${company_id}`}><h5>{$_("menu")}</h5></a>
+        <div
+          class="review-buttons"
+          style={`--review-buttons-width: ${review_buttons_width};
+          margin-right: ${$reviews.length > 0 ? "30px" : "0px"};
+          padding-right: ${$reviews.length > 0 ? "0px" : "30px"}
+        `}
+        >
+          {#if $reviews.length > 0}
+            <Button
+              onClick={() => goto(`/shop/${company_id}/reviews`)}
+              title={$_("all_reviews")}
+              variant="borderless"
+            />
+          {:else}
+            <div class="row center">
+              <p>{$_("no_reviews")}</p>
+            </div>
+          {/if}
+          <Button
+            onClick={() => goto(`/shop/${company_id}/reviews/form`)}
+            title={`+ ${$_("add_review")}`}
+            variant="black"
+          />
+        </div>
+      </section>
     </div>
-  </div>
 
-  {#if $ids_with_images.length + $ids_with_videos.length < 1}
-    <div class="row no-image">
-      <p>{$_("no_images")}</p>
-      <a href={`/shop/${company_id}`}><h5>{$_("check_menu")}</h5></a>
+    <div class="right-desktop-section">
+      <div class="desktop-products" bind:this={products_ref}>
+        <Products {company_id} />
+      </div>
+
+      <section class="menu" bind:this={menu_ref}>
+        <div class="input-container">
+          <h2>Menu</h2>
+          <input
+            class="input-transparent"
+            type="text"
+            placeholder={$_("menu_search_placeholder")}
+            bind:value={$query}
+          />
+        </div>
+
+        <div class="tag-container">
+          {#each all_tags as tag}
+            <Button
+              title={tag}
+              active={tag === $query}
+              variant="black"
+              onClick={() => {
+                if ($query === tag) {
+                  $query = "";
+                } else {
+                  $query = tag;
+                }
+              }}
+            />
+          {/each}
+        </div>
+
+        <Masonry
+          stretchFirst={false}
+          gridGap={"10"}
+          colWidth={"minmax(Min(33%, 220px), 1fr)"}
+          items={$query.length > 1
+            ? Object.values($filtered_ids)
+            : Object.values($grouped_by_tags).flatMap((x) => x)}
+        >
+          {#if $query.length > 1}
+            {#each Object.values($filtered_ids) as product_id}
+              <ProductCard {company_id} product={$products_dic[product_id]} />
+            {/each}
+          {:else}
+            {#each all_tags as tag_name}
+              {#if $grouped_by_tags[tag_name]}
+                {#each $grouped_by_tags[tag_name] as product_id}
+                  <ProductCard
+                    {company_id}
+                    product={$products_dic[product_id]}
+                  />
+                {/each}
+              {/if}
+            {/each}
+          {/if}
+        </Masonry>
+      </section>
     </div>
-  {/if}
-
-  <Masonry
-    stretchFirst={false}
-    gridGap={"10"}
-    colWidth={"minmax(Min(50%, 225px), 1fr)"}
-    items={[...$ids_with_videos, ...$ids_with_images]}
-  >
-    {#each $ids_with_videos as product_id}
-      <Video
-        id={$products_dic[product_id].id}
-        name={$products_dic[product_id].name}
-        price={$products_dic[product_id].price}
-        video_public_id={$products_dic[product_id].videos[0]?.public_id}
-      />
-    {/each}
-
-    {#each $ids_with_images as product_id}
-      <Card
-        {company_id}
-        product={$products_dic[product_id]}
-        show_media={true}
-      />
-    {/each}
-  </Masonry>
-
-  <div class="row center">
-    <a href={`/shop/${company_id}`} class="button">{$_("more_products")}</a>
-  </div>
-</main>
+  </main>
+</div>
 
 <style>
   :global(body) {
-    padding: 0px;
+    padding: 0;
+  }
+
+  .logo {
+    margin-top: 10px;
+    padding-left: 20px;
   }
 
   main {
-    margin-top: 10px;
-    padding-right: 5px;
+    padding-left: 30px;
+  }
+
+  .left-desktop-section {
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
   }
 
   h1 {
@@ -103,59 +228,153 @@
     justify-content: space-between;
     align-items: center;
     padding: 0;
+    margin-top: 0px;
   }
 
   .header {
-    padding: 30px 50px;
+    padding-right: 30px;
   }
 
-  .row {
+  .header p {
+    margin: 0;
+    color: var(--gray);
+  }
+
+  .reviews {
+    margin: 0 0 20px 0;
+    height: 170px;
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
+    overflow-x: scroll;
+    gap: 30px;
   }
 
-  .center {
-    justify-content: center;
+  .review {
+    flex: 0 0 auto;
+    width: 230px;
   }
 
-  .no-image {
+  .review-buttons {
+    width: var(--review-buttons-width);
+    flex: 0 0 auto;
+    display: flex;
     flex-direction: column;
+    justify-content: space-between;
   }
 
-  .pagination {
-    position: fixed;
-    bottom: 20px;
-    right: 30px;
-    z-index: 99;
-    font-size: 18px;
+  .menu {
+    display: none;
+  }
+
+  .input-container {
+    display: flex;
+    justify-content: space-between;
+    margin: 40px 0;
+  }
+
+  .input-container h2 {
+    font-size: 30px;
+    margin: 0;
+    margin-right: 20px;
+  }
+
+  .input-transparent {
+    font-size: 16px;
     border: none;
-    outline: none;
-    background-color: #f0efeb;
-    color: #000000;
-    cursor: pointer;
-    padding: 15px;
-    border-radius: 4px;
+    background-color: #f3f3f4;
+    background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 20 20'><path fill='%23868590' d='M13.22 14.63a8 8 0 1 1 1.41-1.41l4.29 4.29a1 1 0 1 1-1.41 1.41l-4.29-4.29zm-.66-2.07a6 6 0 1 0-8.49-8.49 6 6 0 0 0 8.49 8.49z'></path></svg>");
+    background-repeat: no-repeat;
+    background-position: 10px 10px;
+    background-size: 15px 15px;
+    border-radius: 5px;
+    width: 300px;
+    padding: 0.5em 1em 0.5em 2.5em;
+    margin-bottom: 0px;
   }
 
-  .button {
-    background-color: rgba(51, 51, 51, 0.05);
-    border-radius: 8px;
-    border-width: 0;
-    color: #333333;
-    cursor: pointer;
-    display: inline-block;
-    font-size: 14px;
-    font-weight: 500;
-    line-height: 20px;
-    margin: 1rem 0 1rem 0;
-    padding: 10px 12px;
-    text-align: center;
-    transition: all 200ms;
-    white-space: nowrap;
-    user-select: none;
-    touch-action: manipulation;
-    text-transform: capitalize;
+  .input-transparent::placeholder {
+    color: #868590;
+  }
+
+  .input-transparent:focus {
+    outline: none;
+    background: #f9f9fa;
+    text-shadow: none;
+    background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 20 20'><path fill='%238C92A0' d='M13.22 14.63a8 8 0 1 1 1.41-1.41l4.29 4.29a1 1 0 1 1-1.41 1.41l-4.29-4.29zm-.66-2.07a6 6 0 1 0-8.49-8.49 6 6 0 0 0 8.49 8.49z'></path></svg>");
+    background-repeat: no-repeat;
+    background-position: 10px 10px;
+    background-size: 15px 15px;
+  }
+
+  .tag-container {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    overflow-x: auto;
+    margin-bottom: 20px;
+  }
+
+  .desktop-products {
+    display: none;
+  }
+
+  .mobile-products {
+    display: block;
+  }
+
+  @media (min-width: 768px) {
+    main {
+      width: 900px;
+      margin: 0 auto;
+      display: flex;
+      flex-direction: row;
+    }
+
+    .left-desktop-section {
+      width: 300px;
+    }
+
+    .right-desktop-section {
+      display: flex;
+      flex-direction: column;
+      gap: 25px;
+    }
+
+    .header {
+      padding: 0;
+    }
+
+    .reviews {
+      height: 100%;
+      overflow-x: auto;
+      flex-direction: column;
+      gap: 10px;
+    }
+
+    .menu {
+      display: block;
+      margin-left: 100px;
+      max-height: 600px;
+      overflow-y: auto;
+      overflow-x: hidden;
+    }
+
+    .desktop-products {
+      display: block;
+    }
+
+    .mobile-products {
+      display: none;
+    }
+
+    .reviews .review {
+      width: 100%;
+    }
+
+    .review-buttons {
+      width: 100%;
+      margin: 0 !important;
+      padding: 0 !important;
+      gap: 30px;
+    }
   }
 </style>
---
+++ src/routes/shop/[company_id]/images/Products.svelte
@@ -0,0 +1,93 @@
+<script>
+  import { _ } from "svelte-i18n";
+  import {
+    ids_with_media,
+    ids as products_ids,
+    dic as products_dic,
+  } from "../../../../stores/products";
+  import Carrousel from "../../../../components/carrousel/Carrousel.svelte";
+  import Button from "../../../../components/button/Button.svelte";
+  import { goto } from "$app/navigation";
+
+  export let company_id;
+
+  //products section
+  $: products_with_media = $ids_with_media.map((id) => $products_dic[id]);
+  $: products_count = $products_ids.length;
+</script>
+
+<section class="products">
+  <div class="info">
+    <strong>Featured</strong>
+    {#if products_with_media.length === 0}
+      <div>
+        <span>{$_("no_images")}</span>
+      </div>
+    {:else}
+      <p>
+        Total {products_count} items found. Click the
+        <span class="blue">blue button</span> to check the full menu.
+      </p>
+    {/if}
+  </div>
+
+  {#if products_with_media.length > 0}
+    <Carrousel products={products_with_media} {company_id} variant="shadow" />
+  {/if}
+
+  <div class="row center">
+    <Button
+      onClick={() => goto(`/shop/${company_id}`)}
+      title={`+ ${products_count - products_with_media.length} more items`}
+      variant="blue"
+    />
+  </div>
+</section>
+
+<style>
+  .products {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+
+  .products .info strong {
+    font-size: 18px;
+  }
+
+  .products .info p {
+    margin: 0;
+    font-size: 14px;
+    padding-right: 30px;
+  }
+
+  .blue {
+    color: var(--blue);
+  }
+
+  .row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .center {
+    justify-content: center;
+  }
+
+  @media (min-width: 768px) {
+    .products {
+      padding: 0;
+      margin-left: 100px;
+    }
+
+    .products .info p {
+      display: none;
+    }
+
+    .products .row.center {
+      display: none;
+    }
+  }
+</style>
