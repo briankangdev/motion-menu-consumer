a675087d6d4fd768e838c9e65e5c091694588dbf
IanMuler <yoelmuler@gmail.com>
2023-07-12 16:53:48 -0300

    Create profiling page

---
+++ src/components/profiling-form/ProfilingForm.svelte
@@ -0,0 +1,177 @@
+<script lang="ts">
+  import { _ } from "svelte-i18n";
+  import Button from "../button/Button.svelte";
+
+  type Category = "pizza" | "coffee" | "stakes";
+
+  export let createShop: (shop_name: string) => void;
+
+  const categories: Category[] = ["pizza", "coffee", "stakes"];
+  let category_selected: Category = "pizza";
+  let shop_name: string = "";
+
+  let error: boolean = false;
+  let error_message: string;
+  $: error_message = $_(`routes.shop.profiling.input_error.no_special`);
+
+  function checkInput() {
+    if (!/^[a-zA-Z0-9- ]*$/.test(shop_name)) {
+      error_message = $_(`routes.shop.profiling.input_error.no_special`);
+      error = true;
+      return;
+    }
+    if (shop_name.length < 3 || shop_name.length > 30) {
+      error_message = $_(`routes.shop.profiling.input_error.length`);
+      error = true;
+      return;
+    }
+    error = false;
+    error_message = "";
+  }
+
+  function handleInput(event: Event) {
+    shop_name = (event.target as HTMLInputElement).value;
+  }
+
+  function handleSubmit() {
+    checkInput();
+    if (error) {
+      return;
+    } else {
+      createShop(shop_name);
+    }
+  }
+</script>
+
+<form class="profiling_form" on:submit|preventDefault={handleSubmit}>
+  <div class="shop_name_input">
+    <label for="shop_name">{$_("routes.shop.profiling.input_label")}</label>
+    <input
+      type="text"
+      id="shop_name"
+      data-testid="shop-name-input"
+      bind:value={shop_name}
+      placeholder={$_("routes.shop.profiling.input_placeholder")}
+      on:input={handleInput}
+      on:blur={checkInput}
+    />
+    {#if error}
+      <p class="error_message" data-testid="error-message">
+        {error_message}
+      </p>
+    {/if}
+  </div>
+  <div class="profiling_categories">
+    {#each categories as category}
+      <div
+        class={`profiling_category ${
+          category_selected === category ? "checked" : ""
+        }`}
+      >
+        <input
+          type="radio"
+          name="category"
+          id={category}
+          value={category}
+          on:click={() => {
+            category_selected = category;
+          }}
+          checked={category_selected === category}
+        />
+        <label for={category}>
+          {$_(`routes.shop.profiling.categories.${category}`)}
+        </label>
+      </div>
+    {/each}
+  </div>
+  <div class="profiling_submit">
+    <Button
+      title={$_("routes.shop.profiling.button")}
+      variant="black"
+      test_id="submit-button"
+    />
+  </div>
+</form>
+
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+  }
+
+  .profiling_form {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+
+  .shop_name_input {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .shop_name_input label {
+    font-weight: 600;
+    display: var(--mobile-display);
+  }
+
+  .shop_name_input input {
+    height: 50px;
+    padding: 20px 15px;
+    border-radius: 8px;
+    color: rgb(51, 51, 51);
+    background-color: rgb(235, 235, 235);
+    border: none;
+  }
+
+  input:focus {
+    outline: none;
+    border: 1px solid var(--blue_highlighted);
+  }
+
+  .error_message {
+    color: var(--red);
+    font-weight: 600;
+  }
+
+  .profiling_categories {
+    display: flex;
+    flex-direction: column;
+    text-transform: capitalize;
+    gap: 7px;
+    padding-left: 10px;
+  }
+
+  .profiling_category {
+    display: flex;
+    align-items: center;
+
+    gap: 5px;
+  }
+
+  .profiling_category input {
+    margin: 0;
+    cursor: pointer;
+  }
+
+  .profiling_category label {
+    cursor: pointer;
+    font-size: 17px;
+    letter-spacing: 0.5px;
+  }
+
+  .profiling_category.checked label {
+    font-weight: 600;
+  }
+
+  .profiling_submit {
+    margin: 0 auto;
+  }
+
+  @media (min-width: 1024px) {
+    label {
+      display: block;
+    }
+  }
+</style>
---
+++ src/routes/shop/[company_id]/profiling/+page.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+  import { _ } from "svelte-i18n";
+  import { goto } from "$app/navigation";
+  import { company, type ICompany } from "../../../../stores/company";
+  // import { PROFILING_PAGE } from "../../../../lib/analytics/types";
+  // import analytics from "../../../../lib/analytics";
+  import Logo from "../../../../components/Logo.svelte";
+  import toast from "svelte-french-toast";
+  import ProfilingForm from "../../../../components/profiling-form/ProfilingForm.svelte";
+
+  const createShop = async (shop_name: string) => {
+    if (true) {
+      toast.success($_("routes.shop.profiling.success_message"));
+      goto(`/shop/${$company.id}/images/loading`);
+    } else {
+      toast.error($_("routes.shop.profiling.error_message"));
+    }
+  };
+
+  const handleButtonTrack = (button_name: string) => {
+    // analytics.track.buttonClick(PROFILING_PAGE, button_name, {
+    //   company_id: $company.id,
+    // });
+  };
+</script>
+
+<svelte:head>
+  <title>{$_("routes.shop.profiling.head_title")}</title>
+  <meta name="description" content={$_("routes.shop.profiling.description")} />
+</svelte:head>
+
+<main>
+  <header>
+    <Logo />
+  </header>
+
+  <section>
+    <div class="profiling_header">
+      <h1 data-testid="profiling-title">
+        {$_("routes.shop.profiling.title")}
+      </h1>
+      <p class="description">
+        {$_("routes.shop.profiling.description")}
+      </p>
+    </div>
+    <ProfilingForm {createShop} />
+  </section>
+</main>
+
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+  }
+
+  header {
+    padding: 4px 0 4px 12px;
+  }
+
+  section {
+    padding: 0 30px;
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+  }
+
+  .profiling_header {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  h1 {
+    font-weight: 600;
+    line-height: 1.2;
+    font-size: 45px;
+    word-break: break-word;
+  }
+
+  @media (min-width: 1024px) {
+    header {
+      padding: 30px 0 30px 50px;
+    }
+
+    section {
+      width: 400px;
+      margin: 0 auto;
+      gap: 30px;
+    }
+
+    h1 {
+      font-size: 50px;
+    }
+  }
+</style>
