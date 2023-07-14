e43bb98cf96183995c22879bb0c98f87435db688
IanMuler <70335515+IanMuler@users.noreply.github.com>
2023-07-14 16:40:52 -0300

    Create profiling page (#17)
    * Create profiling page
    
    * Add translations
    
    * Change onClick on Button component
    
    * Tests e2e for profiling page (shop name validations)
    
    * Tests e2e - remove dynamic translation

---
+++ src/routes/shop/[company_id]/profiling/+page.ts
@@ -0,0 +1,15 @@
+import { loadReviewsByPage } from "../../../../services/reviews_service";
+import { getCompany } from "../../../../services/profile_service";
+import type { ICompany } from "src/stores/company";
+
+interface IRouteParams {
+  params: {
+    company_id: string;
+  };
+}
+
+export async function load({ params }: IRouteParams) {
+  let company_slug: ICompany["slug"] = params.company_id;
+
+  await getCompany(company_slug);
+}
