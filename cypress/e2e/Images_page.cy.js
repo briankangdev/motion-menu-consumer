4a1537b225b9af35495d58bb85aa913682b20a85
IanMuler <yoelmuler@gmail.com>
2023-08-02 15:59:34 -0300

    Fix tests

--- cypress/e2e/Images_page.cy.js
+++ cypress/e2e/Images_page.cy.js
@@ -13,64 +13,64 @@ describe("Images Page", () => {
       cy.visit(`/shop/${TEST_COMPANY_ID}/images`);
     });
 
-    // it("render page title", () => {
-    //   cy.wait("@get_company").then(({ response }) => {
-    //     cy.get("[data-testid=company-name]").should(
-    //       "have.text",
-    //       response.body.data.name
-    //     );
-    //   });
-    // });
+    it("render page title", () => {
+      cy.wait("@get_company").then(({ response }) => {
+        cy.get("[data-testid=company-name]").should(
+          "have.text",
+          response.body.data.name
+        );
+      });
+    });
 
-    // it("render company description", () => {
-    //   cy.wait("@get_company").then(({ response }) => {
-    //     cy.get("[data-testid=company-description]").should(
-    //       "have.text",
-    //       response.body.data.description
-    //     );
-    //   });
-    // });
+    it("render company description", () => {
+      cy.wait("@get_company").then(({ response }) => {
+        cy.get("[data-testid=company-description]").should(
+          "have.text",
+          response.body.data.description
+        );
+      });
+    });
 
-    // context("images", () => {
-    //   it("render image carrousel", () => {
-    //     cy.get("[data-testid=carrousel]").should("not.have.length", 0);
-    //   });
+    context("images", () => {
+      it("render image carrousel", () => {
+        cy.get("[data-testid=carrousel]").should("not.have.length", 0);
+      });
 
-    //   it("render 'more items' button", () => {
-    //     cy.get("[data-testid=more-items]").should("exist");
-    //   });
-    // });
+      it("render 'more items' button", () => {
+        cy.get("[data-testid=more-items]").should("exist");
+      });
+    });
 
-    // context("reviews", () => {
-    //   it("render reviews", () => {
-    //     cy.get("[data-testid=review-container]").should("not.have.length", 0);
-    //   });
+    context("reviews", () => {
+      it("render reviews", () => {
+        cy.get("[data-testid=review-container]").should("not.have.length", 0);
+      });
 
-    //   it("show no reviews when there are 0 reviews", () => {
-    //     cy.intercept("GET", `/api/v1/${TEST_COMPANY_ID}/reviews?*`, {
-    //       data: [],
-    //     }).as("get_no_reviews");
+      it("show no reviews when there are 0 reviews", () => {
+        cy.intercept("GET", `/api/v1/${TEST_COMPANY_ID}/reviews?*`, {
+          data: [],
+        }).as("get_no_reviews");
 
-    //     cy.wait("@get_no_reviews").then(() => {
-    //       cy.get("[data-testid=review-container]").should("have.length", 0);
-    //       cy.get("[data-testid=no-reviews]").should("exist");
-    //     });
-    //   });
+        cy.wait("@get_no_reviews").then(() => {
+          cy.get("[data-testid=review-container]").should("have.length", 0);
+          cy.get("[data-testid=no-reviews]").should("exist");
+        });
+      });
 
-    //   it("render 'add review' button", () => {
-    //     cy.get("[data-testid=add-review]").should("exist");
-    //   });
+      it("render 'add review' button", () => {
+        cy.get("[data-testid=add-review]").should("exist");
+      });
 
-    //   it("render 'all reviews' button", () => {
-    //     cy.get("[data-testid=all-reviews]").should("exist");
-    //   });
-    // });
+      it("render 'all reviews' button", () => {
+        cy.get("[data-testid=all-reviews]").should("exist");
+      });
+    });
 
-    // context("menu", () => {
-    //   it("render menu masonry", () => {
-    //     cy.get("[data-testid=menu]").should("exist");
-    //   });
-    // });
+    context("menu", () => {
+      it("render menu masonry", () => {
+        cy.get("[data-testid=menu]").should("exist");
+      });
+    });
 
     context("notification", () => {
       it("does not show success notification when comes from other page", () => {
