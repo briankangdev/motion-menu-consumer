9d3a0462c066f5808cae36ce649985de28ca8ea2
IanMuler <yoelmuler@gmail.com>
2023-08-11 18:32:52 -0300

    Make localhost "https"

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import mkcert from "vite-plugin-mkcert";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), mkcert()],
   test: {
     environment: "jsdom", // necessary for rendering svelte components in tests
     include: ["src/**/*.{test,spec}.{js,ts}"],
+  },
+  server: {
+    https: true,
   },
 };
 
