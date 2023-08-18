bc1d0d2215ed8d7be009640581f726ab3ac439ef
IanMuler <yoelmuler@gmail.com>
2023-08-18 16:43:13 -0300

    Remove wrong google sign in button from Navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,8 +2,6 @@
   import analytics from "../lib/analytics";
   import Logo from "./Logo.svelte";
   import { _ as t } from "svelte-i18n";
-
-  export let profile_name: string | undefined;
 </script>
 
 <header>
@@ -16,27 +14,12 @@
       on:click={() => handleButtonTrack("sign-up")}>{$t("sign_up")}</a
     > -->
 
-    {#if !profile_name}
-      <a
-        href="https://admin.motion.menu/sign_in"
-        on:click={() => {
-          analytics.track("navbar.sign-in-button.click");
-        }}>{$t("sign_in")}</a
-      >
-      <div>
-        <form class="auth-form" method="POST" action="?/OAuth2">
-          <button class="btn-auth" type="submit">
-            <img
-              class="btn-auth-img"
-              src="https://developers.google.com/identity/images/btn_google_signin_light_normal_web.png"
-              alt="Sign in with Google"
-            />
-          </button>
-        </form>
-      </div>
-    {:else}
-      <span>{profile_name}</span>
-    {/if}
+    <a
+      href="https://admin.motion.menu/sign_in"
+      on:click={() => {
+        analytics.track("navbar.sign-in-button.click");
+      }}>{$t("sign_in")}</a
+    >
   </div>
 </header>
 
@@ -59,21 +42,5 @@
 
   a {
     margin-right: 0.8em;
-  }
-
-  .btn-auth {
-    width: 170px;
-    border: none;
-    background: none;
-    padding: 0;
-    cursor: pointer;
-  }
-
-  .btn-auth-img {
-    width: 100%;
-  }
-
-  .btn-auth:focus {
-    background: none;
   }
 </style>
