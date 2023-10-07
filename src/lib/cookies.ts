084059d37f7060f8888587dfdecd2e54f4edb819
Brian Kang <qmfkdldks@gmail.com>
2023-10-07 14:08:47 -0300

    Get from static

--- src/lib/cookies.ts
+++ src/lib/cookies.ts
@@ -1,4 +1,6 @@
 import Cookies from "js-cookie";
-import { PUBLIC_COOKIES_DOMAIN } from "$env/dynamic/public";
+import { PUBLIC_COOKIES_DOMAIN } from "$env/static/public";
+
+const domain = PUBLIC_COOKIES_DOMAIN || undefined;
 
 export default Cookies.withAttributes({ domain: PUBLIC_COOKIES_DOMAIN });
