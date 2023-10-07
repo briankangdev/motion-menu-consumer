696671006ec798519088ca10cbe3210e834c288d
Brian Kang <qmfkdldks@gmail.com>
2023-10-07 14:02:41 -0300

    Update to dynamic

--- src/lib/cookies.ts
+++ src/lib/cookies.ts
@@ -1,4 +1,4 @@
 import Cookies from "js-cookie";
-import { PUBLIC_COOKIES_DOMAIN } from "$env/static/public";
+import { PUBLIC_COOKIES_DOMAIN } from "$env/dynamic/public";
 
 export default Cookies.withAttributes({ domain: PUBLIC_COOKIES_DOMAIN });
