cdcc09873b9f7238195a4c334ab58166ff474560
IanMuler <yoelmuler@gmail.com>
2023-11-10 15:49:14 -0300

    Config partytown

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import { partytownVite } from "@builder.io/partytown/utils";
+import path from "path";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit(),
+    partytownVite({
+      dest: path.join(__dirname, "dist", "~partytown"),
+    }),
+  ],
   test: {
     environment: "jsdom", // necessary for rendering svelte components in tests
     include: ["src/**/*.{test,spec}.{js,ts}"],
