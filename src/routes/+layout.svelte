eae0f56d5f950549a9d6f5837c960d3afd8f5ee0
IanMuler <70335515+IanMuler@users.noreply.github.com>
2023-11-15 15:01:15 -0300

    Improve lighthouse metrics (#34)
    * Add partytown as dependency; update i18n
    
    * Config partytown
    
    * Add partytown to layout and replace google tag manager script
    
    * Fix attempt to change facebook script
    
    * .
    
    * Remove wrong import
    
    * Remove comments

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
   import { onMount } from "svelte";
   import { FacebookPixel, fb } from "@beyonk/svelte-facebook-pixel";
   import Footer from "../components/Footer.svelte";
-  import { install } from "ga-gtag";
+  import { partytownSnippet } from "@builder.io/partytown/integration";
 
   onMount(async () => {
     // user class needs to be instantiated before it can be used in the store
@@ -19,11 +19,29 @@
       fb.track("PageView");
     }
 
-    install("G-W3DG7SGZ9E", { send_page_view: false });
   });
 
   const FACEBOOK_DATASET_ID = "2803302726481229";
 </script>
+
+<svelte:head>
+  <script>
+    partytown = {
+      forward: ["dataLayer.push"],
+    };
+  </script>
+
+  {@html "<script>" + partytownSnippet() + "</script>"}
+
+  <!-- Global Tag Manager (PartyTown) -->
+  <script type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-W3DG7SGZ9E"></script>
+	<script type="text/partytown">
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'G-W3DG7SGZ9E');
+	</script>
+</svelte:head>
 
 <div class="app">
   <FacebookPixel pixels={[FACEBOOK_DATASET_ID]} />
