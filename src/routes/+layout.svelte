d087532f16ae40fc2864ab232972a8cc33dcabbd
Brian Kang <briank@scribd.com>
2024-08-29 11:49:36 -0300

    move install statement

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,8 @@
   import { partytownSnippet } from "@builder.io/partytown/integration";
   import { install } from "ga-gtag";
 
+  install('G-W3DG7SGZ9E');
+
   onMount(async () => {
     // user class needs to be instantiated before it can be used in the store
     if (!$user) {
@@ -19,8 +21,6 @@
     if (fb) {
       fb.track("PageView");
     }
-
-    install('G-W3DG7SGZ9E');
   });
 
   const FACEBOOK_DATASET_ID = "2803302726481229";
